4a03300ea3e61f3271aa7fb300e06b4e46f60ef0
jarudiaz <jazminrdv.2002@gmail.com>
2024-08-25 20:11:04 -0400

    #5 Documentacion

--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def home(request):
+  """
+    Vista para la página de inicio.
+
+    Esta vista renderiza la página de inicio del sitio web.
+
+    Parámetros:
+    -----------
+    request : HttpRequest
+        La solicitud HTTP que se recibe.
+
+    Returns:
+    --------
+    HttpResponse
+        La respuesta HTTP que contiene la página de inicio.
+    """
   return render(request, 'home/home.html')
