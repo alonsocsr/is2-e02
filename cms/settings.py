44d64886d0b0a9e6a6d9bc180fa5213134803fb2
MaiaVillalba <maiafernanda2002@fpuna.edu.py>
2024-08-27 13:13:02 -0400

    #10 navbar y sidebar

--- cms/settings.py
+++ cms/settings.py
@@ -162,6 +162,3 @@ ACCOUNT_RATE_LIMITS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-)
