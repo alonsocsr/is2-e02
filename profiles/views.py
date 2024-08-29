dbc7431728befaaf85f4f81c2d9b38d092d16b6a
MaiaVillalba <maiafernanda2002@fpuna.edu.py>
2024-08-29 08:42:09 -0400

    #8 perfil de usuario

--- profiles/views.py
+++ profiles/views.py
@@ -3,11 +3,21 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import ProfileForm
 from .models import Profile
+from django.contrib import messages
    
    
 class UpdateProfile(LoginRequiredMixin, FormView):
   template_name = "profiles/profile.html"
   form_class = ProfileForm
+
+  def get_initial(self):
+    initial = super().get_initial()
+    user = self.request.user
+    initial['first_name'] = user.first_name
+    initial['last_name'] = user.last_name
+    initial['username'] = user.username
+    #initial['image'] = user.profile.image
+    return initial
 
   def form_valid(self, form):
       user = self.request.user
@@ -23,7 +33,7 @@ class UpdateProfile(LoginRequiredMixin, FormView):
         profile.save()
       
       user.save()
-      
+      messages.success(self.request, "Perfil actualizado con éxito.")
       return super().form_valid(form)
   
   def get_success_url(self):
