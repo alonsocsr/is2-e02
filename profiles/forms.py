e58cf123ca6850f733b6b35fbbfc4a1bf3b5c6f6
césar <cesaralonso99@gmail.com>
2024-08-28 21:23:18 -0400

    #8 Perfil de usuario update form

---
+++ profiles/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+class ProfileForm(forms.Form):
+  first_name = forms.CharField(
+    max_length=150,
+    label= 'Nombres'
+  )
+  last_name = forms.CharField(
+    max_length=150,
+    label= 'Apellidos'
+  )
+  username = forms.CharField(
+    max_length=150,
+    label= 'Nombre de usuario',
+    required=False
+  )
+  image = forms.ImageField(
+    label= 'Foto de Perfil',
+    required=False
+  )
