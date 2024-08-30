01607dcc7a82288e57aaa5d9402d82ffcbe078bc
césar <cesaralonso99@gmail.com>
2024-08-30 13:25:31 -0400

    signal para registrar ak admin

--- profiles/models.py
+++ profiles/models.py
@@ -26,6 +26,11 @@ def create_user_profile(sender, instance, created, **kwargs):
   if created:
     Profile.objects.create(user=instance)
   
-  group = Group.objects.get(name='Suscriptor')
-  instance.groups.add(group)
-  instance.profile.save()
+  if instance.id == 1:
+    group = Group.objects.get(name='Admin')
+    instance.groups.add(group)
+    instance.profile.save()
+  else:
+    group = Group.objects.get(name='Suscriptor')
+    instance.groups.add(group)
+    instance.profile.save()
