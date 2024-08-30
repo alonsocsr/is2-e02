6f6a7b219e61720e8f139a73f07556640b4ae45f
MaiaVillalba <maiafernanda2002@fpuna.edu.py>
2024-08-30 13:35:54 -0400

    #9 remover rol

--- permissions/urls.py
+++ permissions/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("asignarRol/", views.asignar_rol_usuario, name='asignar_rol'),
     path("modificar/<int:rol_id>/", views.modificar_rol, name='modificar_rol'),
     path('eliminar/<int:rol_id>/', views.eliminar_rol, name='eliminar_rol'),
+    path('modificar_usuario/<int:user_id>/', views.modificar_usuario, name='modificar_usuario'),
+    path('modificar_usuario/', views.modificar_usuario, name='modificar_usuario'),
 ]
