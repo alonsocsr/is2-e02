e700b31cc4b67fe7b702e67e26845825adf117c2
césar <cesaralonso99@gmail.com>
2024-09-06 15:12:14 -0400

    #12 Contenido, modificar borrador, versiones y enviar a edicion añadido

--- content/forms.py
+++ content/forms.py
@@ -1,28 +1,44 @@
 from django import forms
 from .models import Contenido
 from categories.models import Categorias
+from django.utils.text import slugify
 
 class ContenidoForm(forms.ModelForm):
     class Meta:
         model = Contenido
-        fields = ['titulo', 'imagen', 'cuerpo', 'categoria']
+        fields = ['titulo', 'resumen', 'imagen', 'cuerpo', 'categoria', 'fecha_publicacion', 'vigencia', 'slug']
         labels = {
-            'titulo': 'Titulo',
-            'imagen': 'Selecciona una Imagen',
-            'cuerpo': 'Cuerpo del Contenido',
-            'categoria': 'Categoria',
+            'titulo': 'Titulo:',
+            'resumen': 'Resumen:',
+            'imagen': 'Imagen Principal:',
+            'cuerpo': 'Cuerpo del Contenido:',
+            'categoria': 'Categoria:',
+            'fecha_publicacion': 'Fecha de Publicación:',
+            'vigencia': 'Fecha de vigencia:',
         }
         widgets = {
-            'autor': forms.HiddenInput()
+            'fecha_publicacion': forms.DateInput(attrs={'type': 'date'}),
+            'vigencia': forms.DateInput(attrs={'type': 'date'}),
+            'slug': forms.HiddenInput,
         }
 
     def __init__(self, *args, **kwargs):
         autor = kwargs.pop('autor', None)
 
         super(ContenidoForm, self).__init__(*args, **kwargs)
+        self.fields['slug'].required = False
         self.fields['categoria'].queryset = Categorias.objects.all()
         if autor is not None:
             self.initial['autor'] = autor
+        
+    def clean_slug(self):
+        instance = self.instance
+        slug = slugify(self.cleaned_data['titulo'])
+        if instance.pk and instance.slug == slug:
+            return slug
+        if Contenido.objects.filter(slug=slug).exists():
+            raise forms.ValidationError('Este Título ya está en uso. Elige otro.')
+        return slug
 
     def save(self, commit=True):
         contenido = super(ContenidoForm, self).save(commit=False)
@@ -30,4 +46,5 @@ class ContenidoForm(forms.ModelForm):
             contenido.autor = self.initial['autor']
         if commit:
             contenido.save()
+            contenido.save_version()
         return contenido
--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CrearContenido
+from .views import CrearContenido, ContenidoBorradorList, CambiarEstadoView
 
 
 urlpatterns = [
-    path("crear_contenido/", CrearContenido.as_view(), name='crear_contenido'),
+    path("mis-borradores/", ContenidoBorradorList.as_view(), name='lista_borradores'),
+    path("editar/<int:contenido_id>/", CrearContenido.as_view(), name='crear_contenido'),
+    path("crear-contenido/", CrearContenido.as_view(), name='crear_contenido_nuevo'),
+    path('cambiar-estado/<int:pk>/', CambiarEstadoView.as_view(), name='cambiar_estado'),
 ]
