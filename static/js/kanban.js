0f66abd196e7e407197e04b814a602e8e37b9e9e
MaiaVillalba <maiafernanda2002@fpuna.edu.py>
2024-10-25 10:31:16 -0300

    #59 update front

--- static/js/kanban.js
+++ static/js/kanban.js
@@ -148,6 +148,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function ocultarModalConfirmacion() { confirmModal.classList.add('hidden'); }
 
     confirmarCambiosBtn.addEventListener('click', function () {
+        
         confirmarCambiosBtn.disabled = true;
         confirmarCambiosBtn.classList.add('bg-pink-900', 'cursor-not-allowed');
         confirmarCambiosBtn.classList.remove('bg-pink-700');
