4566a7f6e36cbe53ce54384f0297427395999142
césar <cesaralonso99@gmail.com>
2024-12-04 21:47:18 -0300

    fix: static

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -146,7 +146,7 @@ USE_I18N = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
