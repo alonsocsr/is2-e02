8699c8211e5f68ff5df6c3044a8c74f613f5c17e
césar <cesaralonso99@gmail.com>
2024-12-07 11:43:55 -0300

    update: profile image

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -74,7 +74,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'allauth.account.middleware.AccountMiddleware',
 ]
